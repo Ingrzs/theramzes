abf447e13da52f95a1c6b25dcc4763da016471c2
Ramirozs <94188197+Ingrzs@users.noreply.github.com>
2025-09-25 17:27:23 -0600

    Update config.js

--- config.js
+++ config.js
@@ -1,11 +1,1 @@
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-export const firebaseConfig = {
-    apiKey: "AIzaSyB-GfHrjdFiFEQG1hb38TyAl1X7AIqarnM",
-    authDomain: "theramzes-creations.firebaseapp.com",
-    projectId: "theramzes-creations",
-    storageBucket: "theramzes-creations.firebasestorage.app",
-    messagingSenderId: "497450013723",
-    appId: "1:497450013723:web:1d3019c9c0d7da82a754be",
-    measurementId: "G-1B2TVSMM1Y"
-};
+
